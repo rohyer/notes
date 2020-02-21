985ffd8610c8a15b973c9b83c44790491e044a6d
rohyer <guilhermerl.dev@gmail.com>
2020-02-20 23:33:11 -0300

    Inicio do projeto

---
+++ view/header.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="pt-br">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <link rel="stylesheet" href="./assets/css/style.css">
+
+        <title>Notes</title>
+    </head>
+  <body>
+        <header id="header">
+            <div class="container-fluid">
+                <div class="row">
+                    <div id="header-left" class="col-4">
+                        <div id="hamburger">
+                            <span></span>
+                        </div>
+                        <div id="logo">
+                            <h1>Notes</h1>
+                        </div>
+                    </div>
+
+                    <div id="header-right" class="col-8">
+                        <div id="profile">
+                            <!-- <img src="" alt=""> -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
