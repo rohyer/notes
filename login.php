39c506349d1f994c88556fbdf005e1a27fb19d68
rohyer <guilhermerl.dev@gmail.com>
2020-02-22 17:31:46 -0300

    Página login

---
+++ login.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="./assets/css/style.css">
+
+    <title>Login</title>
+  </head>
+  <body>
+    
+    <div id="login-box">
+        <h1>Notes</h1>
+        <h2>Faça seu login</h2>
+        <form action="" method="post">
+            <input type="email" name="email-login" id="email-login" placeholder="Email" class="info-login">
+            <input type="password" name="password-login" id="password-login" placeholder="Senha" class="info-login">
+            <a href="#" id="forgotten-password">Esqueceu seu email ou senha?</a>
+
+            <a href="./register.php" id="link-register">Criar conta</a>
+            <input type="submit" value="Entrar" name="btnLogar">
+        </form>
+    </div>
+
+    <script src="./assets/js/script.js"></script>
+  </body>
+</html>
---
+++ register.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="./assets/css/style.css">
+
+    <title>Register</title>
+  </head>
+  <body>
+    
+    <script src="./assets/js/script.js"></script>
+  </body>
+</html>
