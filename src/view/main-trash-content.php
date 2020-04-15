c70b3cb6b6c3e0e2f5e190eac07b50b3b3c8041f
rohyer <guilhermerl.dev@gmail.com>
2020-04-14 21:47:30 -0300

    Ajustes básicos da pagina trash

--- src/view/main-trash-content.php
+++ src/view/main-trash-content.php
@@ -14,4 +14,28 @@
     }
     ?>
 
+    <div class="note-container">
+        <?php foreach ($listDeletedNotes as $value) { ?>
+        <div class="single-note">
+            <div class="title-single-note"><?php echo $value['titlenote']; ?></div>
+            <input type="hidden" name="category-single-note" class="category-single-note" value="<?php echo $value['idcategory'] ?>">
+            <?php if ($value['markednote'] == 1) { ?>
+                <div class="marked-single-note"><i class="fas fa-check"></i></div>
+                <input type="hidden" name="idnote" value="<?php echo $value['markednote'] ?>" class="marked-single-note-value" readonly>
+            <?php } ?>
+            <div class="description-single-note"><?php echo $value['descriptionnote']; ?></div>
+            <div class="options-single-note">
+                <div class="colors-options">
+                    <i class="fas fa-palette"></i>
+                </div>
+                <div data-toggle="modal" data-target="#delete-note-modal" data-id="<?php echo $value['idnote']; ?>" class="delete-note">
+                    <i class="fas fa-trash-alt"></i>
+                </div>
+            </div>
+        </div>
+        <?php } ?>
+
+    </div>
+    
+
 </section>
